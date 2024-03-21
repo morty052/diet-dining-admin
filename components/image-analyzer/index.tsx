affe716776dac78b37261426ff8fcb1c036f56dd
morty052 <anomaly619052@gmail.com>
2024-03-21 09:00:32 +0100

    feat:refined genie

--- components/image-analyzer/index.tsx
+++ components/image-analyzer/index.tsx
@@ -30,12 +30,14 @@ import Colors from "../../constants/colors";
 import { SEMI_BOLD } from "../../constants/fontNames";
 import BackButton from "../ui/BackButton";
 import * as ImagePicker from "expo-image-picker";
+import { sanityClient } from "../../utils/sanityClient";
+import { UploadBody } from "@sanity/client";
 
 type Props = {};
 
 const Stack = createNativeStackNavigator();
 
-const IdentifyMealWithCamScreen = (props: Props) => {
+const IdentifyMealWithCamScreen = ({ navigation }) => {
   const device = useCameraDevice("back", {
     physicalDevices: ["ultra-wide-angle-camera"],
   });
@@ -50,6 +52,7 @@ const IdentifyMealWithCamScreen = (props: Props) => {
   const [video, setVideo] = React.useState<VideoFile>();
 
   const [breakDown, setBreakDown] = React.useState<null | string>(null);
+  const [loading, setLoading] = React.useState(false);
 
   const camera = React.useRef<Camera>(null);
 
@@ -77,26 +80,41 @@ const IdentifyMealWithCamScreen = (props: Props) => {
   };
 
   const uploadPhoto = async () => {
-    if (!photo) {
+    if (!photo || loading) {
       return;
     }
 
-    const res = await fetch(`file://${photo.path}`);
-    const arraybuffer = await res.arrayBuffer();
-    const fileExt = photo.path?.split(".").pop()?.toLowerCase() ?? "jpeg";
-    const path = `${Date.now()}.${fileExt}`;
-    const { data, error: uploadError } = await supabase.storage
-      .from("temp_images/public")
-      .upload(path, arraybuffer, {
-        contentType: "image/jpg",
-      });
+    setLoading(true);
+    try {
+      const res = await fetch(`file://${photo.path}`);
+      const arraybuffer = await res.arrayBuffer();
+      const fileExt = photo.path?.split(".").pop()?.toLowerCase() ?? "jpeg";
+      const path = `${Date.now()}.${fileExt}`;
+      const { data, error: uploadError } = await supabase.storage
+        .from("temp_images/public")
+        .upload(path, arraybuffer, {
+          contentType: "image/jpg",
+        });
 
-    const url = await getImageurl(data?.path);
+      const url = await getImageurl(data?.path);
 
-    const geminiRes = await fetch(`${baseUrl}/meal?url=${url}`);
-    const geminidata = await geminiRes.json();
-    console.log(geminidata);
-    setBreakDown(geminidata.data);
+      const geminiRes = await fetch(`${baseUrl}/meal?url=${url}`);
+      const geminidata = await geminiRes.json();
+      if (geminidata.data.error) {
+        setLoading(false);
+        throw new Error(geminidata.error);
+      }
+      console.log(geminidata);
+      const breakdown = {
+        nutritional_breakdown: geminidata.data.nutritional_breakdown,
+        ingredients: geminidata.data.ingredients,
+      };
+      setLoading(false);
+      navigation.navigate("analysis", breakdown);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
   };
 
   if (!hasPermission) {
@@ -137,13 +155,13 @@ const IdentifyMealWithCamScreen = (props: Props) => {
             source={{ uri: `file://${photo.path}` }}
             style={StyleSheet.absoluteFill}
           />
-          <Ionicons
+          {/* <Ionicons
             onPress={() => setPhoto(undefined)}
             name="arrow-back"
             size={25}
             color="white"
             style={{ position: "absolute", top: 50, left: 30 }}
-          />
+          /> */}
           <View
             style={{
               position: "absolute",
@@ -152,18 +170,34 @@ const IdentifyMealWithCamScreen = (props: Props) => {
               right: 0,
               paddingBottom: 50,
               backgroundColor: "rgba(0, 0, 0, 0.40)",
+              alignItems: "center",
             }}
           >
-            <Button title="Upload" onPress={uploadPhoto} />
+            <Pressable
+              onPress={uploadPhoto}
+              style={{
+                alignItems: "center",
+                justifyContent: "center",
+                backgroundColor: Colors.light,
+                width: "95%",
+                height: 50,
+                borderRadius: 10,
+                marginBottom: 50,
+              }}
+            >
+              {!loading && (
+                <Text style={{ color: "black", fontSize: 20 }}>Analyse</Text>
+              )}
+              {loading && <ActivityIndicator size={"large"} color={"black"} />}
+            </Pressable>
           </View>
         </>
       )}
-      {breakDown && breakDown?.ingredients.map((i) => <Text>{i}</Text>)}
     </View>
   );
 };
 
-const UploadMealScreen = () => {
+const UploadMealScreen = ({ navigation }) => {
   const [preview, setPreview] = React.useState<null | string>(null);
   const [photo, setPhoto] = React.useState<null | any>(null);
   const [loading, setLoading] = React.useState(false);
@@ -186,7 +220,7 @@ const UploadMealScreen = () => {
   };
 
   const uploadPhoto = async () => {
-    if (!photo) {
+    if (!photo || loading) {
       return;
     }
     setLoading(true);
@@ -201,11 +235,28 @@ const UploadMealScreen = () => {
           contentType: "image/jpg",
         });
 
-      const url = await getImageurl(data?.path);
+      if (uploadError) {
+        console.log(uploadError);
+        setLoading(false);
+        return;
+      }
 
+      const url = await getImageurl(data?.path);
+      console.log(url);
       const geminiRes = await fetch(`${baseUrl}/meal?url=${url}`);
       const geminidata = await geminiRes.json();
-      console.log(geminidata);
+
+      if (!geminidata.data.error) {
+        setLoading(false);
+        throw new Error(geminidata.error);
+      }
+
+      const breakdown = {
+        nutritional_breakdown: geminidata.data.nutritional_breakdown,
+        ingredients: geminidata.data.ingredients,
+      };
+
+      navigation.navigate("analysis", breakdown);
       setLoading(false);
     } catch (error) {
       console.error(error);
@@ -322,6 +373,78 @@ const PickIDTypeScreen = () => {
   );
 };
 
+const AnalysisScreen = ({ route }) => {
+  const { ingredients, nutritional_breakdown } = route.params ?? {};
+
+  const nutritional_array = nutritional_breakdown
+    ? Object.entries(nutritional_breakdown).map(([key, value]) => ({
+        key: key,
+        value,
+      }))
+    : [];
+
+  return (
+    <Screen>
+      <View style={{ gap: 30 }}>
+        <View>
+          <Text
+            style={{
+              fontSize: 30,
+              color: Colors.light,
+              fontFamily: SEMI_BOLD,
+              borderBottomWidth: 1,
+              borderColor: Colors.accent,
+              paddingBottom: 5,
+              marginBottom: 10,
+            }}
+          >
+            Ingredients
+          </Text>
+          {ingredients?.map((ingredient: string, index: number) => (
+            <Text style={{ fontSize: 20, color: Colors.light }} key={index}>
+              {ingredient}
+            </Text>
+          ))}
+        </View>
+        <View>
+          <Text
+            style={{
+              fontSize: 30,
+              color: Colors.light,
+              fontFamily: SEMI_BOLD,
+              borderBottomWidth: 1,
+              borderColor: Colors.accent,
+              paddingBottom: 5,
+            }}
+          >
+            Nutritional Value
+          </Text>
+          {nutritional_array?.map((item: any, index: number) => (
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-between",
+                alignItems: "center",
+                paddingVertical: 20,
+                borderBottomWidth: 1,
+                borderColor: Colors.light,
+              }}
+              key={index}
+            >
+              <Text style={{ fontSize: 20, color: Colors.light }}>
+                {item.key}
+              </Text>
+              <Text style={{ fontSize: 20, color: Colors.light }}>
+                {item.value}
+              </Text>
+            </View>
+          ))}
+        </View>
+      </View>
+    </Screen>
+  );
+};
+
 const IdentifyMealScreen = (props: Props) => {
   return (
     <Stack.Navigator
@@ -336,6 +459,7 @@ const IdentifyMealScreen = (props: Props) => {
       <Stack.Screen name="pick" component={PickIDTypeScreen} />
       <Stack.Screen name="cam" component={IdentifyMealWithCamScreen} />
       <Stack.Screen name="upload" component={UploadMealScreen} />
+      <Stack.Screen name="analysis" component={AnalysisScreen} />
     </Stack.Navigator>
   );
 };
